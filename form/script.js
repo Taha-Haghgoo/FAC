3eea1f7713c51d877c7f6d5db48de3a11b70bb4f
Taha <93734684+Taha-Haghgoo@users.noreply.github.com>
2022-12-23 12:16:00 -0800

    update input livedate picker

--- form/script.js
+++ form/script.js
@@ -3,6 +3,23 @@ document.querySelector("#livedate").value = today;
 
 var submit = document.getElementById("submit")
 
-submit.addEventListener("click" , () => {
- window.alert("data stored");
-});
+if(submit){
+    submit.addEventListener("click" , () => {
+      console.log("get")
+    });
+}
+  // JQuery
+  $(document).ready( function() {
+    $('#liveDate').val(new Date().toDateInputValue());
+  });
+  
+  // Pure JS
+  document.getElementById('livedate').value = new Date().toDateInputValue();
+  
+  
+  // Timezone support
+  Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+  });
